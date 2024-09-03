533527f0b4276a692ee22d4a3bce7e61b3d7f5e1
samshawon10 <shawonakando518@gmail.com>
2024-09-04 00:26:46 +0600

    added javascript Practice code file

---
+++ introduction to Javascript/first.js
@@ -0,0 +1,24 @@
+
+var weight = 23;
+var price = 23;
+var balance = 200;
+var money = 2005;
+var age = 23;
+
+console.log(age)
+console.log(money)
+console.log(balance)
+
+Name = "Shawon";
+address = "Sadar, Mymensingh"
+country = "Bangladesh"
+console.log(Name)
+console.log(address)
+console.log(country)
+var isRich = true;
+console.log(isRich)
+var passed = false;
+console.log(passed)
+console.log(typeof passed)
+console.log(typeof country)
+console.log(typeof age)
